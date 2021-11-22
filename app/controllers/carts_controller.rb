5d7db7ff21bd10205d6a23545229f993dab2173f
justinl <cas022101@coderacademy.edu.au>
2021-11-22 22:48:20 +1100

    Added stripe for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,10 +6,44 @@ class CartsController < ApplicationController
   def create
     if @cart
       @cart.items << @item
+      flash[:alert] = "Item added to cart!"
     end
   end
 
   def show
+    if @cart.items.length == 0
+      flash[:alert] = "Cart empty!"
+    end
+
+    @line_items = []
+
+    @cart.items.each do |i|
+      @line_items << {
+      name: i.title,
+      description: i.description,
+      images: [i.picture],
+      amount: i.price,
+      currency: 'jpy',
+      quantity: 1
+      }
+    end
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      customer_email: current_user.email,
+
+      line_items: [@line_items],
+      payment_intent_data: {
+        metadata: {
+          cart_id: @cart.id
+        }
+      },
+      success_url: "#{root_url}payments/success?itemId=#{@cart.id}",
+      cancel_url: "#{root_url}items"
+    )
+
+    @session_id = session.id
+
   end
 
   private
