d8c33af42a9bee736a67f9866a31eb2bf0f32cf0
justinl <cas022101@coderacademy.edu.au>
2021-11-25 12:19:50 +1100

    Migration default values for heroku

--- db/migrate/20211118103041_add_item_ref_to_materials.rb
+++ db/migrate/20211118103041_add_item_ref_to_materials.rb
@@ -1,5 +1,5 @@
 class AddItemRefToMaterials < ActiveRecord::Migration[6.0]
   def change
-    add_reference :materials, :item, null: false, foreign_key: true
+    add_reference :materials, :item, null: false, foreign_key: true,  default: 1
   end
 end
--- db/migrate/20211118103053_add_item_ref_to_types.rb
+++ db/migrate/20211118103053_add_item_ref_to_types.rb
@@ -1,5 +1,5 @@
 class AddItemRefToTypes < ActiveRecord::Migration[6.0]
   def change
-    add_reference :types, :item, null: false, foreign_key: true
+    add_reference :types, :item, null: false, foreign_key: true,  default: 1
   end
 end
