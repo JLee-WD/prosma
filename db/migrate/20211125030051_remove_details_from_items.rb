c10a1b404150182b1152e30ff3b128c84242b7c5
justinl <cas022101@coderacademy.edu.au>
2021-11-26 03:00:28 +1100

    Migration fix for heroku

--- db/migrate/20211125030051_remove_details_from_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDetailsFromItems < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :items, :first_name, :string, null: false, default: ""
-    remove_column :items, :last_name, :string, null: false, default: ""
-  end
-end
