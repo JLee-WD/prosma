c1376749ced49ddfa0141700dd13f8ef4f27e9a5
justinl <cas022101@coderacademy.edu.au>
2021-11-25 12:26:19 +1100

    Migration update for heroku

--- db/migrate/20211119051201_add_limb_ref_to_items.rb
+++ db/migrate/20211119051201_add_limb_ref_to_items.rb
@@ -1,5 +1,5 @@
 class AddLimbRefToItems < ActiveRecord::Migration[6.0]
   def change
-    add_reference :items, :limb, null: false, foreign_key: true, default: 1
+    add_reference :items, :limb, null: false, foreign_key: true
   end
 end
